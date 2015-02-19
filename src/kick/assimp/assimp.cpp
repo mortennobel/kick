484fd344a964b3bb0e66365b17748ed145f61aad
Morten Nobel-Jørgensen <morten@nobel-joergensen.com>
2015-02-19 15:18:59 +0100

    Improve ass imp

--- src/kick/assimp/assimp.cpp
+++ src/kick/assimp/assimp.cpp
@@ -30,11 +30,20 @@ namespace kick {
             return glm::vec3{v.x,v.y,v.z};
         }
 
+        glm::vec2 toVec2(aiVector2D v){
+            return glm::vec2{v.x,v.y};
+        }
+
+        glm::vec2 toVec2(aiVector3D v){
+            return glm::vec2{v.x,v.y};
+        }
+
         glm::vec3 toVec3(aiColor3D v){
             return glm::vec3{v.r,v.g,v.b};
         }
 
         void importNode(aiNode * node, Scene *scenePtr, Transform* parent, std::map<aiNode *,GameObject*>& nodeMap){
+            logInfo(std::string("Import node ")+node->mName.C_Str());
             for (int i=0;i<node->mNumChildren;i++){
                 aiNode * childNode = node->mChildren[i];
                 GameObject* go = scenePtr->createGameObject(childNode->mName.C_Str());
@@ -50,12 +59,14 @@ namespace kick {
                 go->transform()->setLocalRotation(glm::quat(rotation.w,rotation.x,rotation.y,rotation.z));
                 go->transform()->setLocalPosition(toVec3(position));
                 go->transform()->setLocalScale(toVec3(scaling));
+                logInfo(std::string("RTS ")+/*glm::to_string(go->transform()->localRotation())+*/glm::to_string(go->transform()->localPosition())+glm::to_string(go->transform()->localScale()));
 
                 importNode(childNode, scenePtr, go->transform(), nodeMap);
             }
         }
 
         void assignMesh(aiNode * node, int meshIndex, std::shared_ptr<Mesh> mesh, Material * pMaterial, std::map<aiNode *,GameObject*>& nodeMap) {
+            logInfo(std::string("Assign mesh ")+node->mName.C_Str());
             for (int i=0;i<node->mNumChildren;i++){
                 aiNode *child = node->mChildren[i];
                 for (int j=0;j<child->mNumMeshes;j++){
@@ -205,12 +216,51 @@ namespace kick {
                 }
             }
             if (importSettings.meshes){
-                for (int i=0;i<scene->mNumMeshes;i++){
-                    aiMesh* aiMesh = scene->mMeshes[i];
+                for (int j=0;j<scene->mNumMeshes;j++){
+                    aiMesh* aiMesh = scene->mMeshes[j];
                     std::shared_ptr<MeshData> meshData = std::make_shared<MeshData>();
                     std::shared_ptr<Mesh> mesh = std::make_shared<Mesh>();
+                    logInfo(std::string("Mesh ")+aiMesh->mName.C_Str());
+                    if (aiMesh->HasPositions()){
+                        std::vector<glm::vec3> pos;
+                        for (int i=0;i<aiMesh->mNumVertices;i++){
+                            pos.push_back(toVec3(aiMesh->mVertices[i]));
+                        }
+                        meshData->setPosition(pos);
+                    }
+                    if (aiMesh->HasNormals()){
+                        std::vector<glm::vec3> norms;
+                        for (int i=0;i<aiMesh->mNumVertices;i++){
+                            norms.push_back(toVec3(aiMesh->mNormals[i]));
+                        }
+                        meshData->setNormal(norms);
+                    }
+                    if (aiMesh->GetNumUVChannels()>0){
+                        std::vector<glm::vec2> norms;
+                        for (int i=0;i<aiMesh->mNumVertices;i++){
+                            norms.push_back(toVec2(aiMesh->mTextureCoords[0][i]));
+                        }
+                        meshData->setTexCoord0(norms);
+                    }
+                    if (aiMesh->GetNumUVChannels()>1){
+                        std::vector<glm::vec2> norms;
+                        for (int i=0;i<aiMesh->mNumVertices;i++){
+                            norms.push_back(toVec2(aiMesh->mTextureCoords[1][i]));
+                        }
+                        meshData->setTexCoord1(norms);
+                    }
+                    if (aiMesh->HasFaces()){
+                        std::vector<GLushort> indices;
+                        for (int i=0;i<aiMesh->mNumFaces;i++){
+                            for (int ii=0;ii< aiMesh->mFaces[i].mNumIndices;ii++){
+                                indices.push_back(aiMesh->mFaces[i].mIndices[ii]);
+                            }
+                        }
+                        meshData->setSubmesh(0, indices, MeshType::Triangles);
+                    }
+                    meshData->recomputeBounds();
                     mesh->setMeshData(meshData);
-                    assignMesh(scene->mRootNode, i, mesh, mats[aiMesh->mMaterialIndex], nodeMap);
+                    assignMesh(scene->mRootNode, j, mesh, mats[aiMesh->mMaterialIndex], nodeMap);
                 }
             }
         }
@@ -222,9 +272,12 @@ namespace kick {
         // And have it read the given file with some example postprocessing
         // Usually - if speed is not the most important aspect for you - you'll
         // propably to request more postprocessing than we do in this example.
+        logInfo(std::string("Loading ")+filename);
         const aiScene* scene = importer.ReadFile( filename,
                 aiProcess_CalcTangentSpace       |
                         aiProcess_Triangulate            |
+                        aiProcess_GenNormals            |
+                        aiProcess_GenUVCoords            |
                         aiProcess_JoinIdenticalVertices  |
                         aiProcess_SortByPType);
 
@@ -233,9 +286,13 @@ namespace kick {
         {
             logError(importer.GetErrorString());
             return false;
+        } else {
+            logInfo(std::string("Importing ")+filename);
         }
         // Now we can access the file's contents.
         doSceneProcessing(scene, scenePtr, importSettings);
+
+        logInfo(std::string("Finished ")+filename);
 
         // We're done. Everything will be cleaned up by the importer destructor
         return true;
